6f8863428d65e14915406a29a0930b906ff37dbd
Jan Kaiser <jan.kaiser.email@googlmail.com>
2018-04-24 18:21:40 +0200

    Wrote code to read and print .plt file line-by-line

--- Converter.java
+++ Converter.java
@@ -1,7 +1,28 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 class Converter {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world! I am a converter.");
+        String path = "../Geolife Trajectories 1.3/Data/000/Trajectory/20081023025304.plt";
+        File file = new File(path);
+
+        try (BufferedReader r = new BufferedReader(new FileReader(file));) {
+            String line = r.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = r.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Could not find file at path \"" + path + "\".");
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("An I/O exception occured: " + e.getLocalizedMessage());
+            System.exit(1);
+        }
     }
 
 }
