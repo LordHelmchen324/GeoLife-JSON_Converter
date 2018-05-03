e977e8e96158659c40ae7d23dc41f6dd495ad9c7
Jan Kaiser <jan.kaiser.email@googlemail.com>
2018-05-03 12:06:31 +0200

    Converting a smaller data set for now

--- Converter.java
+++ Converter.java
@@ -11,7 +11,7 @@ public class Converter {
     public static void main(String[] args) {
         System.out.println("GeoLife to JSON Converter started ...\n");
 
-        Parser p = new Parser("../Geolife Trajectories 1.3/Data/");
+        Parser p = new Parser("../Geolife Trajectories 1.3/Data Small/");
         Dataset d = p.parseDataset();
 
         System.out.println("\nSize of the dataset = " + d.size() + "\n");
